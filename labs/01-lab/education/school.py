af3dad01f30093a4233e5deccbc26d47ec5e012b
Anton Barash <antonbarash@gmail.com>
2022-09-15 17:35:22 -0400

    Added initial library and school buildings.

--- labs/01-lab/education/school.py
+++ labs/01-lab/education/school.py
@@ -1,3 +1,14 @@
 def draw_school():
-    print("school not found")
+    draw_school_building()
     return
+
+def draw_school_building():
+    print("""                 _ _.-'`-._ _
+                ;.'________'.;
+     _________n.[____________].n_________
+    |""_""_""_""||==||==||==||""_""_""_""]
+    |LI LI LI LI||LI||LI||LI||LI LI LI LI|
+    |.. .. .. ..||..||..||..||.. .. .. ..|
+    |LI LI LI LI||LI||LI||LI||LI LI LI LI|
+ ,,;;,;;;,;;;,;;;,;;;,;;;,;;;,;;,;;;,;;;,;;,,
+;;jgs;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;""")
