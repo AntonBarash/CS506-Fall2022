f4dbda7b4d86089d9b4c21933204a77198221cb8
Anton Barash <antonbarash@gmail.com>
2022-09-22 17:43:47 -0400

    Initial function definitions, seem to pass for some tests but fail for others.

--- labs/02-lab/cs506/read.py
+++ labs/02-lab/cs506/read.py
@@ -1,6 +1,15 @@
+import csv
 def read_csv(csv_file_path):
     """
         Given a path to a csv file, return a matrix (list of lists)
         in row major.
     """
-    raise NotImplementedError()
+    f = open(csv_file_path, 'r')
+    data = csv.reader(f)
+    res = []
+    for row in data:
+        res.append(row)
+    return res
+
+#print(read_csv('dataset_1.csv'))
+    
--- labs/02-lab/cs506/sim.py
+++ labs/02-lab/cs506/sim.py
@@ -5,12 +5,36 @@ def euclidean_dist(x, y):
     return res**(1/2)
 
 def manhattan_dist(x, y):
-    raise NotImplementedError()
+    res = 0
+    for i in range(len(x)):
+        res += abs(x[i] - y[i])
+    return res
 
 def jaccard_dist(x, y):
-    raise NotImplementedError()
+    if len(x) == 0:
+        return 1
+    res = 0
+    for i in range(len(x)):
+        if x[i] == y[i]:
+            res += 1
+    return 1 - (res/len(x))
 
 def cosine_sim(x, y):
-    raise NotImplementedError()
+    if len(x) == 0:
+        return 1
+    if len(y) == 0:
+        return 1
+    dotprod = 0
+    magnofx = 0
+    magnofy = 0
+    for i in range(min(len(x), len(y))):
+        dotprod += x[i] * y[i]
+        magnofx += x[i] * x[i]
+        magnofy += y[i] * y[i]
+    magnofx = magnofx ** (0.5)
+    magnofy = magnofy ** (0.5)
+    if magnofx == 0 or magnofy == 0:
+        return 0
+    return dotprod / (magnofx * magnofy)
 
 # Feel free to add more
